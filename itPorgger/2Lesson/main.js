9e476d830cf16b8ddfd0158515b062a6d2755e16
ReNothing <pashasob2009@gmail.com>
2025-07-12 14:24:07 +0300

    Lesson 2

---
+++ itPorgger/2Lesson/main.js
@@ -0,0 +1,15 @@
+/* document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! ");
+document.write("Hello, World! "); */
+
+console.log("Hello, World!");
+console.info("Hello, World!");
+console.error("Hello, World!");
+console.warn("Hello, World!");
