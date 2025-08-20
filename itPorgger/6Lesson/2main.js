f9b377d99cb811229b8b367bbfc6bb2e09457162
ReNothing <pashasob2009@gmail.com>
2025-08-20 11:26:11 +0300

    6Lesson

---
+++ itPorgger/6Lesson/2main.js
@@ -0,0 +1,15 @@
+var str = "word";
+
+switch(str) {
+    case "4": console.log("4 пальца") //if (str == "4")
+    break;
+
+    case "word": console.log("ворд. хохохохох. шутка") //if (str == "word")
+    break;
+
+    default: //else
+        console.log("else")
+        console.log("lol")
+        console.log("mango 💀😭")
+    break;
+}
---
+++ itPorgger/6Lesson/main.js
@@ -0,0 +1,19 @@
+var number = 15;
+var meet = true
+// && - И
+if (number > 5 || meet == true) {
+    console.log(true)
+} else if(number == 15) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+number = "5"
+if (number == "5") {
+    console.log(true)
+}
+
+if(meet == true) {
+    console.log(true)
+}
