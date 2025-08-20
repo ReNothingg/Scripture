81af335bad11031fc0dbe9ea9409257c7764dfd2
ReNothing <pashasob2009@gmail.com>
2025-08-20 10:58:17 +0300

    4Lesson

---
+++ itPorgger/4Lesson/main.js
@@ -0,0 +1,16 @@
+// var num;
+// num = 5;
+
+const num = 5;
+// num = 7;
+console.log("Переменная: " + num + ".");
+
+var number;
+number = "Words. N";
+number = true
+number = 5.5
+
+number = "string"
+
+var num_1 = 5;
+var num_2 = "5"
