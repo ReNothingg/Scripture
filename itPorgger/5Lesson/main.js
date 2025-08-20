66099ae2be64a2c98176754caf918f3497258ba7
ReNothing <pashasob2009@gmail.com>
2025-08-20 11:10:45 +0300

    5Lesson

---
+++ itPorgger/5Lesson/main.js
@@ -0,0 +1,27 @@
+var num_1 = 5;
+var num_2 = 15;
+
+// var res = num_1 - num_2
+
+console.log("Вычитание: " + (num_1 - num_2))
+console.log("Сложение: " + (num_1 + num_2))
+console.log("Деление: " + (num_1 / num_2))
+console.log("Умножение: " + (num_1 * num_2))
+console.log("Остаток: " + (num_1 % num_2))
+
+var num_3 = 5;
+num_3 = num_1 + 7;
+num_3 += 7;
+num_3++;
+num_3--;
+console.log("Результат " + num_3)
+
+var str_1 = Number("12"); //str, ToString
+var str_2 = Number("2");
+console.log("Результат " + (str_1 + str_2))
+
+console.log("Math PI: " + (Math.PI))
+console.log("Math E: " + (Math.E))
+console.log("Math cos: " + (Math.cos(30)))
+console.log("Math min: " + (Math.min(34, 432, 4, 2, 5, 1, 5145, 5))) //1
+console.log("Math max: " + (Math.max(34, 432, 4, 2, 5, 1, 5145, 5))) //1
