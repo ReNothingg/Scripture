6f3bdc78f5007d7a624270b827e1aaa7e42ccb9d
ReNothing <pashasob2009@gmail.com>
2025-08-22 18:35:02 +0300

    Удалены неиспользуемые файлы 2main.js и 3main.js, добавлен новый обработчик ввода в index.html и улучшен стиль кнопки в main.js.

--- itPorgger/11Lesson/main.js
+++ itPorgger/11Lesson/main.js
@@ -4,8 +4,15 @@ function onClickBtn(el) {
     counter++;
     console.log(counter);
     el.innerHTML = "Press: " + counter;
+    el.style.background = "blue";
 
-    console.log()
+    el.style.cssText = "font-size: 202px;"
 }
-
 //stoped 14:42
+
+function OnInput(el) {
+    if(el.valve == "Hello") {
+        console.log("И тебе Hello!");
+    }
+    console.log(el.valve);
+}
