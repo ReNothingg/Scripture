32ede69d6a416dc5f28fee69f8c965d53358aa21
ReNothing <pashasob2009@gmail.com>
2025-08-22 19:49:45 +0300

    Обновлено содержание README.md: добавлены новые размышления о проекте, улучшено описание практики и уточнены цели использования репозитория.

---
+++ itPorgger/14Lesson/2main.js
@@ -0,0 +1,13 @@
+var arr = [7, 5, 34, 3, 0, 2, 23]
+
+var stroka = arr.reverse().join(", ")
+// console.log(arr.length);
+// console.log(arr.join(", "));
+
+// console.log(arr.sort());
+// console.log(arr.reverse());
+// console.log(arr.reverse().join(", "));
+
+console.log(stroka.split(", "));
+
+//stoped 11:43
---
+++ itPorgger/14Lesson/main.js
@@ -0,0 +1,11 @@
+var date = new Date();
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth() + 1);
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+
+// date.setHours(23);
+// date.setMinutes(23);
+
+console.log("Время: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
